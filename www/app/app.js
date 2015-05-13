8b6aa7c9b296f33deeaf29bdfa6fcbf35561d711
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-05-13 16:52:05 +0100

    that'll do, Donkey

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-angular.module('geoTracker', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('geoTracker', ['ionic'])
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -16,61 +16,13 @@ angular.module('geoTracker', ['ionic', 'starter.controllers', 'starter.services'
 
   .config(function ($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
 
-      // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "app/tabs/tabs.html"
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'app/dash/dash.html',
-            controller: 'DashCtrl'
-          }
-        }
-      })
-
-      .state('tab.chats', {
-        url: '/chats',
-        views: {
-          'tab-chats': {
-            templateUrl: 'app/chats/chats.html',
-            controller: 'ChatsCtrl'
-          }
-        }
-      })
-      .state('tab.chat-detail', {
-        url: '/chats/:chatId',
-        views: {
-          'tab-chats': {
-            templateUrl: 'app/chats/chat-detail.html',
-            controller: 'ChatDetailCtrl'
-          }
-        }
-      })
-
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'app/account/account.html',
-            controller: 'AccountCtrl'
-          }
-        }
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html'
       });
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    $urlRouterProvider.otherwise('/home');
 
   });
