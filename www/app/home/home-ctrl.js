85044a19c1ab6689830d8223e3ddad4241da211d
Daniel Hollands <daniel@limeblast.co.uk>
2015-05-14 20:03:49 +0100

    geolocation is a go

--- www/app/home/home-ctrl.js
+++ www/app/home/home-ctrl.js
@@ -1,15 +1,34 @@
 (function () {
   'use strict';
 
-  angular.module('geoTracker').controller('HomeCtrl', ['$ionicPlatform', '$cordovaDevice', HomeCtrl]);
+  angular.module('geoTracker').controller('HomeCtrl', ['$ionicPlatform', '$cordovaDevice', '$cordovaGeolocation', HomeCtrl]);
 
-  function HomeCtrl($ionicPlatform, $cordovaDevice) {
+  function HomeCtrl($ionicPlatform, $cordovaDevice, $cordovaGeolocation) {
     var vm = this;
 
     vm.example = 'This proves data binding works';
 
     $ionicPlatform.ready(function () {
       vm.device = $cordovaDevice.getDevice();
+
+
+      var watchOptions = {
+        frequency: 1000,
+        timeout: 3000,
+        enableHighAccuracy: false // may cause errors if true
+      };
+
+      var watch = $cordovaGeolocation.watchPosition(watchOptions);
+      watch.then(
+        null,
+        function (err) {
+          // error
+        },
+        function (position) {
+          vm.lat = position.coords.latitude;
+          vm.long = position.coords.longitude;
+        });
+
     });
 
 
