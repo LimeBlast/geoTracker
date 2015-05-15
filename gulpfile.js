15d911ba79e5490b41ff90c4e11ac21f2e1b2c02
Daniel Hollands <daniel@limeblast.co.uk>
2015-05-15 21:56:00 +0100

    Revert "added ng-annotate (not sure why)"
    This reverts commit 49bd4e3beff0cca4f8bc1ee2394a696c567bb950.

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,13 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 var templateCache = require('gulp-angular-templatecache');
-var ngAnnotate = require('gulp-ng-annotate');
 
 var paths = {
   sass: ['./scss/**/*.scss'],
-  templatecache: ['./www/templates/**/*.html'],
-  ng_annotate: ['./www/app/**/*.js']
+  templatecache: ['./www/templates/**/*.html']
 };
 
-gulp.task('default', ['sass', 'templatecache', 'ng_annotate']);
+gulp.task('default', ['sass', 'templatecache']);
 
 gulp.task('sass', function (done) {
   gulp.src('./scss/ionic.app.scss')
@@ -34,7 +32,6 @@ gulp.task('sass', function (done) {
 gulp.task('watch', function () {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.templatecache, ['templatecache']);
-  gulp.watch(paths.ng_annotate, ['ng_annotate']);
 });
 
 gulp.task('install', ['git-check'], function () {
@@ -61,12 +58,5 @@ gulp.task('templatecache', function (done) {
   gulp.src('./www/app/**/*.html')
     .pipe(templateCache({standalone: true}))
     .pipe(gulp.dest('./www/app'))
-    .on('end', done);
-});
-
-gulp.task('ng_annotate', function (done) {
-  gulp.src('./www/app/**/*.js')
-    .pipe(ngAnnotate({single_quotes: true}))
-    .pipe(gulp.dest('./www/dist/dist_js/app'))
     .on('end', done);
 });
